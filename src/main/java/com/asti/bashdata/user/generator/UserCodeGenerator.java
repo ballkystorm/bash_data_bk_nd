9439df51ee012f9e32e1ab722a0bfbe5d440e300
Mubarak <mubarakbnmisbahudeen@gmail.com>
2026-07-05 12:09:40 +0100

    feat(user): add user code generator

--- src/main/java/com/asti/bashdata/user/generator/UserCodeGenerator.java
+++ src/main/java/com/asti/bashdata/user/generator/UserCodeGenerator.java
@@ -1,4 +1,30 @@
 package com.asti.bashdata.user.generator;
 
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * Generates unique user codes.
+ */
+@Component
 public class UserCodeGenerator {
+
+    private static final String PREFIX = "USR-";
+
+    /**
+     * Generates a unique user code.
+     *
+     * @return generated user code
+     */
+    public String generate() {
+
+        return PREFIX +
+                UUID.randomUUID()
+                        .toString()
+                        .replace("-", "")
+                        .substring(0, 8)
+                        .toUpperCase();
+    }
+
 }
